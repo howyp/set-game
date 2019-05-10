ffec45cc12b0e656c9f07541dbf736b5418785cb
Howy Perrin <howyp@users.noreply.github.com>
2019-05-10 16:47:18 +0100

    Add Colour feature and generalise tests out to handle all features

--- src/main/scala/itv/exercise/Card.scala
+++ src/main/scala/itv/exercise/Card.scala
@@ -1,10 +1,22 @@
 package itv.exercise
 
-sealed trait Shading
-object Shading {
+sealed trait FeatureValue
+sealed trait Feature { def allValues: (FeatureValue, FeatureValue, FeatureValue) }
+
+sealed trait Shading extends FeatureValue
+object Shading extends Feature {
   case object Solid    extends Shading
   case object Striped  extends Shading
   case object Outlined extends Shading
+  val allValues = (Solid, Striped, Outlined)
+}
+
+sealed trait Colour extends FeatureValue
+object Colour extends Feature {
+  case object Red    extends Colour
+  case object Purple extends Colour
+  case object Green  extends Colour
+  val allValues = (Red, Purple, Green)
 }
 
 case class Card(shading: Shading)
---
+++ src/test/scala/itv/exercise/FeatureSpec.scala
@@ -0,0 +1,66 @@
+package itv.exercise
+
+import org.scalacheck.{Arbitrary, Gen, Shrink}
+import Arbitrary.arbitrary
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FreeSpec, Matchers}
+import org.scalacheck.magnolia._
+
+import scala.util.Random
+
+class FeatureSpec extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks {
+  implicit def noShrink[T]: Shrink[T] = Shrink(_ => Stream.empty)
+
+  def test(feature1: FeatureValue, feature2: FeatureValue, feature3: FeatureValue): Boolean =
+    feature1 == feature2 && feature2 == feature3 ||
+      feature1 != feature2 && feature2 != feature3 && feature1 != feature3
+
+  "A card feature" - {
+    "is valid for a Set if" - {
+      "they are all the same" in forAll { featureValue: FeatureValue =>
+        test(
+          feature1 = featureValue,
+          feature2 = featureValue,
+          feature3 = featureValue
+        ) should be(true)
+      }
+      "they are different in all three cards" in {
+        val differentFeatureValuesInRandomOrder = arbitrary[Feature].map(_.allValues).map(shuffleTuple)
+
+        forAll(differentFeatureValuesInRandomOrder) {
+          case (_1, _2, _3) =>
+            test(
+              feature1 = _1,
+              feature2 = _2,
+              feature3 = _3
+            ) should be(true)
+        }
+      }
+    }
+    "is invalid if they are the same for two but different for the third" in {
+      val differentFeatureValuesInRandomOrder = for {
+        feature <- arbitrary[Feature]
+        _1      <- Gen.oneOf(asList(feature.allValues))
+        _2      <- Gen.oneOf(asList(feature.allValues)) if _1 != _2
+      } yield shuffleTuple((_1, _1, _2))
+
+      forAll(differentFeatureValuesInRandomOrder) {
+        case (_1, _2, _3) =>
+          test(
+            feature1 = _1,
+            feature2 = _2,
+            feature3 = _3
+          ) should be(false)
+      }
+    }
+  }
+
+  def shuffleTuple[T](v: (T, T, T)): (T, T, T) =
+    Random.shuffle(asList(v)) match {
+      case List(_1, _2, _3) => (_1, _2, _3)
+    }
+
+  private def asList[T](v: (T, T, T)) = {
+    List(v._1, v._2, v._3)
+  }
+}
