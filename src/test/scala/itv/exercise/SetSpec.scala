c7a9fbde482105f109a48154fffa3d5e1692b6b8
Howy Perrin <howyp@users.noreply.github.com>
2019-05-10 16:22:14 +0100

    Fix issue highlighted by prop tests where order of cards mattered

--- src/test/scala/itv/exercise/SetSpec.scala
+++ src/test/scala/itv/exercise/SetSpec.scala
@@ -1,6 +1,6 @@
 package itv.exercise
 
-import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.{Arbitrary, Gen, Shrink}
 import Arbitrary.arbitrary
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FreeSpec, Matchers}
@@ -9,9 +9,11 @@ import org.scalacheck.magnolia._
 import scala.util.Random
 
 class SetSpec extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks {
+  implicit def noShrink[T]: Shrink[T] = Shrink(_ => Stream.empty)
+
   def test(card1: Card, card2: Card, card3: Card): Boolean =
     card1.shading == card2.shading && card2.shading == card3.shading ||
-      card1.shading != card2.shading && card2.shading != card3.shading
+      card1.shading != card2.shading && card2.shading != card3.shading && card1.shading != card3.shading
 
   "A Set of three cards" - {
     "is valid if" - {
@@ -38,17 +40,17 @@ class SetSpec extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks 
     }
     "is invalid if" - {
       "a feature is the same for two but different for the third" in {
-        val differentShadings = for {
+        val differentShadingsInRandomOrder = for {
           _1 <- arbitrary[Shading]
           _2 <- arbitrary[Shading] if _1 != _2
-        } yield (_1, _2)
+        } yield Random.shuffle(List(_1, _1, _2))
 
-        forAll(differentShadings) {
-          case (_1, _2) =>
+        forAll(differentShadingsInRandomOrder) {
+          case List(_1, _2, _3) =>
             test(
               card1 = Card(_1),
-              card2 = Card(_1),
-              card3 = Card(_2)
+              card2 = Card(_2),
+              card3 = Card(_3)
             ) should be(false)
         }
       }
