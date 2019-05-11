831be3aeb22313273225eea232d5adc737b27785
Howy Perrin <howyp@users.noreply.github.com>
2019-05-11 11:57:57 +0100

    Add next cards after removing a submitted set

--- src/main/scala/itv/exercise/Game.scala
+++ src/main/scala/itv/exercise/Game.scala
@@ -1,9 +1,14 @@
 package itv.exercise
 
-case class GameState(cardsOnTable: List[Card])
+case class GameState(cardsOnTable: List[Card], remainingCards: List[Card])
 
-class Game(cardsInDealingOrder: List[Card]) {
-  val cardsOnTable = cardsInDealingOrder
+class Game(private var state: GameState) {
+  val cardsOnTable = state.cardsOnTable
   def callSet(card1: Card, card2: Card, card3: Card) =
-    GameState(cardsOnTable.filterNot(Set(card1, card2, card3).contains))
+    GameState(cardsOnTable = cardsOnTable.filterNot(Set(card1, card2, card3).contains) ::: state.remainingCards,
+              remainingCards = cardsOnTable)
+}
+object Game {
+  def apply(cardsInDealingOrder: List[Card]): Game =
+    new Game(GameState(cardsOnTable = cardsInDealingOrder.take(12), remainingCards = cardsInDealingOrder.drop(12)))
 }
--- src/test/scala/itv/exercise/GameSpec.scala
+++ src/test/scala/itv/exercise/GameSpec.scala
@@ -18,6 +18,11 @@ class GameSpec extends FreeSpec with Matchers {
       Card(Green, Two, Striped, Diamonds),
       Card(Purple, One, Solid, Squiggle)
     )
+    val validSet3 = List(
+      Card(Red, One, Outlined, Diamonds),
+      Card(Red, Three, Solid, Squiggle),
+      Card(Red, Two, Striped, Oval)
+    )
     val invalidSet1 = List(
       Card(Purple, One, Solid, Diamonds),
       Card(Red, One, Outlined, Diamonds),
@@ -28,16 +33,17 @@ class GameSpec extends FreeSpec with Matchers {
       Card(Purple, Three, Solid, Squiggle),
       Card(Purple, Three, Outlined, Oval)
     )
-    val shuffledCards = validSet1 ::: invalidSet1 ::: validSet2 ::: invalidSet2
-    val initialGame   = new Game(shuffledCards)
+    val shuffledCards = validSet1 ::: invalidSet1 ::: validSet2 ::: invalidSet2 ::: validSet3
+    val initialGame   = Game(shuffledCards)
 
     "Starts with dealing a the first 12 cards onto the table" in {
       initialGame.cardsOnTable should have(size(12))
-      initialGame.cardsOnTable should be(shuffledCards)
+      initialGame.cardsOnTable should be(validSet1 ::: invalidSet1 ::: validSet2 ::: invalidSet2)
     }
     "Allows a player to submit a valid set, which removes those cards from the table" in {
       val updatedGame = initialGame.callSet(validSet1(0), validSet1(1), validSet1(2))
       updatedGame.cardsOnTable should contain noneOf (validSet1(0), validSet1(1), validSet1(2))
+      updatedGame.cardsOnTable should be(invalidSet1 ::: validSet2 ::: invalidSet2 ::: validSet3)
     }
   }
 }
