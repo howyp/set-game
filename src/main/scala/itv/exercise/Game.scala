b6920aee9834ce1c7e2e4261b120dfb32174c7cd
Howy Perrin <howyp@users.noreply.github.com>
2019-05-11 20:17:55 +0100

    Remove points if set is invalid

--- src/main/scala/itv/exercise/Game.scala
+++ src/main/scala/itv/exercise/Game.scala
@@ -1,18 +1,23 @@
 package itv.exercise
 
+import java.lang.Math.max
+
 case class GameState(cardsOnTable: List[Card], remainingCards: List[Card], points: Map[String, Int])
 
 class Game(private var state: GameState) {
   val cardsOnTable = state.cardsOnTable
   val points       = state.points
 
-  def submitSet(player: String, card1: Card, card2: Card, card3: Card): Either[String, Game] =
+  def submitSet(player: String, card1: Card, card2: Card, card3: Card): Game.SubmitResult =
     if (!CardSet.validate(card1, card2, card3))
-      Left("not a valid set")
+      Game.SubmitResult.Invalid(
+        "not a valid set",
+        new Game(state.copy(points = state.points.updated(player, max(state.points(player) - 1, 0))))
+      )
     else if (!(cardsOnTable.contains(card1) || cardsOnTable.contains(card2) || cardsOnTable.contains(card3)))
-      Left("cards not found on table")
+      Game.SubmitResult.Invalid("cards not found on table", this)
     else
-      Right(
+      Game.SubmitResult.Valid(
         new Game(
           GameState(
             cardsOnTable = cardsOnTable.filterNot(Set(card1, card2, card3).contains) ::: state.remainingCards.take(3),
@@ -23,6 +28,12 @@ class Game(private var state: GameState) {
       )
 }
 object Game {
+  sealed trait SubmitResult { def game: Game }
+  object SubmitResult {
+    case class Valid(game: Game)                   extends SubmitResult
+    case class Invalid(reason: String, game: Game) extends SubmitResult
+  }
+
   def apply(cardsInDealingOrder: List[Card], players: Set[String]): Game =
     new Game(
       GameState(
