0019285601162a95163ffbb8efcf26efc69783ef
Howy Perrin <howyp@users.noreply.github.com>
2019-05-11 10:49:47 +0100

    Tidy up CardSpec

--- src/test/scala/itv/exercise/CardSpec.scala
+++ src/test/scala/itv/exercise/CardSpec.scala
@@ -11,75 +11,71 @@ import scala.util.Random
 class CardSpec extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks {
 
   "Three cards are" - {
-    def validCombinationFor(feature: Feature) = Gen.oneOf(
-      Gen.oneOf(asList3(feature.allValues)).map(v => (v, v, v)),
-      Gen.const(feature.allValues).map(shuffleTuple)
-    )
-    def invalidCombinationFor(feature: Feature) =
-      for {
-        first  <- Gen.oneOf(asList3(feature.allValues))
-        second <- Gen.oneOf(asList3(feature.allValues)) if first != second
-      } yield shuffleTuple((first, first, second))
-    def combinationFor(feature: Feature)(valid: Boolean): Gen[(feature.Value, feature.Value, feature.Value)] =
-      if (valid) validCombinationFor(feature) else invalidCombinationFor(feature)
-
-    "a valid set if all features are valid" in forAll(
-      validCombinationFor(Colour),
-      validCombinationFor(Number),
-      validCombinationFor(Shading),
-      validCombinationFor(Shape)
-    ) {
-      case ((colour1, colour2, colour3),
-            (number1, number2, number3),
-            (shading1, shading2, shading3),
-            (shape1, shape2, shape3)) =>
-        Set.validate(
-          card1 = Card(colour1, number1, shading1, shape1),
-          card2 = Card(colour2, number2, shading2, shape2),
-          card3 = Card(colour3, number3, shading3, shape3)
-        ) should be(true)
+    "a valid set if all features are valid" in forAll(validFeatureCombinations.map(featuresToCards)) {
+      case (card1, card2, card3) => Set.validate(card1, card2, card3) should be(true)
     }
 
-    val atLeastOneInvalidCombination =
-      for {
-        (colourValid, numberValid, shadingValid, shapeValid) <- fourBooleansWithAtLeastOneFalse
-        colour                                               <- combinationFor(Colour)(colourValid)
-        number                                               <- combinationFor(Number)(numberValid)
-        shading                                              <- combinationFor(Shading)(shadingValid)
-        shape                                                <- combinationFor(Shape)(shapeValid)
-      } yield (colour, number, shading, shape)
-
-    "a invalid set if one feature is invalid" in forAll(atLeastOneInvalidCombination) {
-      case ((colour1, colour2, colour3),
-            (number1, number2, number3),
-            (shading1, shading2, shading3),
-            (shape1, shape2, shape3)) =>
-        Set.validate(
-          card1 = Card(colour1, number1, shading1, shape1),
-          card2 = Card(colour2, number2, shading2, shape2),
-          card3 = Card(colour3, number3, shading3, shape3)
-        ) should be(false)
+    "a invalid set if one feature is invalid" in forAll(combinationsWithAtLeastOneInvalidFeature.map(featuresToCards)) {
+      case (card1, card2, card3) => Set.validate(card1, card2, card3) should be(false)
     }
   }
 
-  private def fourBooleansWithAtLeastOneFalse =
-    Gen.zip(Gen.const(false), arbitrary[Boolean], arbitrary[Boolean], arbitrary[Boolean]).map(shuffleTuple4)
+  val validFeatureCombinations = Gen.zip(
+    validCombinationFor(Colour),
+    validCombinationFor(Number),
+    validCombinationFor(Shading),
+    validCombinationFor(Shape)
+  )
 
-  private def shuffleTuple[T](v: (T, T, T)): (T, T, T) =
-    Random.shuffle(asList3(v)) match {
-      case List(_1, _2, _3) => (_1, _2, _3)
-    }
+  val combinationsWithAtLeastOneInvalidFeature =
+    for {
+      (colourValid, numberValid, shadingValid, shapeValid) <- fourBooleansWithAtLeastOneFalseInAnyOrder
+      colour                                               <- combinationFor(Colour)(colourValid)
+      number                                               <- combinationFor(Number)(numberValid)
+      shading                                              <- combinationFor(Shading)(shadingValid)
+      shape                                                <- combinationFor(Shape)(shapeValid)
+    } yield (colour, number, shading, shape)
 
-  private def asList3[T](v: (T, T, T)) = {
-    List(v._1, v._2, v._3)
+  private def combinationFor(feature: Feature)(valid: Boolean): Gen[(feature.Value, feature.Value, feature.Value)] =
+    if (valid) validCombinationFor(feature) else invalidCombinationFor(feature)
+
+  private def validCombinationFor(feature: Feature) = {
+    val allValues                     = feature.allValues
+    val combinationsWhereAllSame      = Gen.oneOf(asList(allValues)).map(v => (v, v, v))
+    val combinationsWhereAllDifferent = Gen.const(allValues).map(shuffleTuple)
+    Gen.oneOf(combinationsWhereAllSame, combinationsWhereAllDifferent)
   }
 
-  private def shuffleTuple4[T](v: (T, T, T, T)): (T, T, T, T) =
-    Random.shuffle(asList4(v)) match {
-      case List(_1, _2, _3, _4) => (_1, _2, _3, _4)
-    }
+  private def invalidCombinationFor(feature: Feature) =
+    for {
+      duplicated    <- Gen.oneOf(asList(feature.allValues))
+      nonDuplicated <- Gen.oneOf(asList(feature.allValues)) if duplicated != nonDuplicated
+    } yield shuffleTuple((duplicated, duplicated, nonDuplicated))
 
-  private def asList4[T](v: (T, T, T, T)) = {
-    List(v._1, v._2, v._3, v._4)
+  private val featuresToCards: (
+      (
+          (Colour.Value, Colour.Value, Colour.Value),
+          (Number.Value, Number.Value, Number.Value),
+          (Shading.Value, Shading.Value, Shading.Value),
+          (Shape.Value, Shape.Value, Shape.Value)
+      )) => (Card, Card, Card) = {
+    case ((colour1, colour2, colour3),
+          (number1, number2, number3),
+          (shading1, shading2, shading3),
+          (shape1, shape2, shape3)) =>
+      (
+        Card(colour1, number1, shading1, shape1),
+        Card(colour2, number2, shading2, shape2),
+        Card(colour3, number3, shading3, shape3)
+      )
   }
+
+  private def fourBooleansWithAtLeastOneFalseInAnyOrder =
+    Gen
+      .zip(Gen.const(false), arbitrary[Boolean], arbitrary[Boolean], arbitrary[Boolean])
+      .map(v => Random.shuffle(List(v._1, v._2, v._3, v._4)))
+      .map { case List(_1, _2, _3, _4) => (_1, _2, _3, _4) }
+
+  private def shuffleTuple[T](v: (T, T, T)) = Random.shuffle(asList(v)) match { case List(_1, _2, _3) => (_1, _2, _3) }
+  private def asList[T](v: (T, T, T))       = List(v._1, v._2, v._3)
 }
